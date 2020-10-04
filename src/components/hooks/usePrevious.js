7f27ed37be2931bef386ecf8db1f11bc90da6e1d
asosunoff <al.sosunoff@gmail.com>
2020-10-04 16:31:04 +0300

    Added usePrevious

---
+++ src/components/hooks/usePrevious.js
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from "react";
+
+export default function usePrevious(value) {
+	const prevValueRef = useRef();
+
+	useEffect(() => {
+		prevValueRef.current = value;
+	});
+
+	return prevValueRef.current;
+}
