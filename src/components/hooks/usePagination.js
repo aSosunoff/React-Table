2871a39b2a5a8d332a8c6b2ce63511e4c5b94841
asosunoff <al.sosunoff@gmail.com>
2020-10-04 19:47:50 +0300

    Added use pagination

---
+++ src/components/hooks/usePagination.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const chunk = (arr, size = 1) => {
+	const a = arr || [];
+
+	return Array(Math.ceil(a.length / size))
+		.fill([])
+		.map((e, inx) => a.slice(inx * size, inx * size + size));
+};
+
+export default function usePagination(value) {
+	const [pagination, setPagination] = useState({
+		current: 1,
+		size: 5,
+		count: 0,
+		allItems: [],
+		itemsOnPage: [],
+	});
+
+	const pageChangeHandler = (page) => {
+		setPagination({
+			...pagination,
+			current: page,
+			itemsOnPage:
+				pagination.allItems[page - 1] || pagination.allItems[0] || [],
+		});
+	};
+
+	const setupPagination = (pageAllItems) => {
+		setPagination({
+			...pagination,
+			allItems: chunk(pageAllItems, pagination.size),
+			count: pagination.allItems.length,
+		});
+		pageChangeHandler(pagination.current);
+	};
+
+	return {
+		current: 1,
+		size: 5,
+		count: 0,
+		allItems: [],
+		itemsOnPage: [],
+		pageChangeHandler,
+		setupPagination,
+	};
+}
