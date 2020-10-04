d2413036b0f8255aaa2286261cdd27da2adff641
asosunoff <al.sosunoff@gmail.com>
2020-10-04 21:30:47 +0300

    fix table container

--- src/components/table/tableContainer/TableContainer.js
+++ src/components/table/tableContainer/TableContainer.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styles from "./TableContainer.module.css";
 
 const TableContainer = ({ header, btnsLength, children }) => {
-	const FILTER_BTN_COUNT = 1;
-	const widthColumn = {
-		"--width-column": Object.values(header)
-			.map(({ width = "1fr" }) => width)
-			.concat(Array(btnsLength || FILTER_BTN_COUNT).fill("50px"))
-			.join(" "),
-	};
+	const memoizedWidthColumn = useMemo(() => {
+		const FILTER_BTN_COUNT = 1;
+
+		return {
+			"--width-column": Object.values(header)
+				.map(({ width = "1fr" }) => width)
+				.concat(Array(btnsLength || FILTER_BTN_COUNT).fill("50px"))
+				.join(" "),
+		};
+	}, [header, btnsLength]);
 
 	return (
-		<div className={styles.table} style={widthColumn}>
+		<div className={styles.table} style={memoizedWidthColumn}>
 			{children}
 		</div>
 	);
