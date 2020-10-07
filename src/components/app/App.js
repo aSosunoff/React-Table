22dfa18021f6509951899460b49c9d609ee48cf0
asosunoff <al.sosunoff@gmail.com>
2020-10-07 10:50:04 +0300

    Added App component

---
+++ src/components/app/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Table from "../table/Table";
+
+function App() {
+	const [list, setList] = useState([
+		{
+			id: 1,
+			text: "a",
+			name: "b",
+		},
+		{
+			id: 2,
+			text: "aa",
+			name: "bb",
+		},
+		{
+			id: 3,
+			text: "aaa",
+			name: "bbb",
+		},
+		{
+			id: 4,
+			text: "aaaa",
+			name: "bbbb",
+		},
+		{
+			id: 5,
+			text: "aaaaa",
+			name: "bbbbb",
+		},
+		/* {
+			id: 6,
+			text: "aaaaaa",
+			name: "bbbbbb",
+		}, */
+	]);
+
+	const added = () => {
+		setList([...list, { name: "111" }]);
+	};
+
+	return (
+		<>
+			<Table
+				title="Таблица"
+				list={list}
+				header={{
+					id: {
+						titleHead: "№",
+						width: "50px",
+						order: true,
+					},
+					text: {
+						titleHead: "текст",
+						order: true,
+					},
+					name: {
+						titleHead: "имя",
+					},
+				}}
+				pageSize={3}
+				/* btns={} */
+			></Table>
+			<button onClick={added}>111</button>
+		</>
+	);
+}
+
+export default App;
