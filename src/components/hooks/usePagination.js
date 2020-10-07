81f60bacb650ca797572a5c511c66da0bac8dbd2
asosunoff <al.sosunoff@gmail.com>
2020-10-07 10:50:20 +0300

    fix use pagination

--- src/components/hooks/usePagination.js
+++ src/components/hooks/usePagination.js
@@ -8,35 +8,61 @@ const chunk = (arr, size = 1) => {
 		.map((e, inx) => a.slice(inx * size, inx * size + size));
 };
 
-export default function usePagination(size) {
+const fillCleanItem = (size, countItems) =>
+	countItems < size ? Array(size - countItems).fill("") : [];
+
+export default function usePagination(size = 5) {
 	const [pagination, setPagination] = useState({
-		current: 1,
 		size,
+		current: 1,
+		itemsOnPage: [],
 		count: 0,
 		allItems: [],
+	});
+
+	/* const [currentPageSetting, setCurrentPage] = useState({
+		current: 1,
 		itemsOnPage: [],
 	});
 
+	const [allItemsSetting, setAllItems] = useState({
+		allItems: [],
+		count: 0,
+	}); */
+
 	const pageChangeHandler = (page) => {
-		setPagination({
-			...pagination,
-			current: page,
-			itemsOnPage:
-				pagination.allItems[page - 1] || pagination.allItems[0] || [],
+		setPagination((pagination) => {
+			const { allItems, size } = pagination;
+			const itemsOnPage = allItems[page - 1] || allItems[0] || [];
+			return {
+				...pagination,
+				current: page,
+				itemsOnPage: [
+					...itemsOnPage,
+					...fillCleanItem(size, itemsOnPage.length),
+				],
+			};
 		});
 	};
 
-	const setupPagination = (pageAllItems) => {
-		setPagination({
-			...pagination,
-			allItems: chunk(pageAllItems, pagination.size),
-			count: pagination.allItems.length,
+	const setupPagination = (list) => {
+		setPagination((pagination) => {
+			const { size } = pagination;
+			const allItems = chunk(list, size);
+
+			return {
+				...pagination,
+				allItems,
+				count: allItems.length,
+			};
 		});
-		pageChangeHandler(pagination.current);
+
+		const { current } = pagination;
+		pageChangeHandler(current);
 	};
 
 	return {
-		...pagination,
+		itemsOnPage: pagination.itemsOnPage,
 		pageChangeHandler,
 		setupPagination,
 	};
