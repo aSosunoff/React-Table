8d18738f8b8ceaa58eb578b90c51f7e0372e8e55
asosunoff <al.sosunoff@gmail.com>
2020-10-09 00:28:50 +0300

    Body fix

--- src/components/table/body/Body.js
+++ src/components/table/body/Body.js
@@ -1,24 +1,13 @@
-import React, { useMemo } from "react";
+import React from "react";
 import styles from "./Body.module.css";
-import { v4 } from "uuid";
-import { cloneDeep } from "lodash";
 import Row from "./Row";
 
 const Body = ({ list = [], header, rowsBtn = [] }) => {
-	const localList = useMemo(
-		() =>
-			cloneDeep(list).map((record) => ({
-				uuid: v4(),
-				...record,
-			})),
-		[list]
-	);
-
 	const row = (record) => Object.keys(header).map((key) => [key, record[key]]);
 
 	return (
 		<div className={styles.table__body}>
-			{localList.map(({ uuid, ...record }, indexRecord) => (
+			{list.map(({ uuid, ...record }, indexRecord) => (
 				<Row
 					key={uuid}
 					row={row(record)}
