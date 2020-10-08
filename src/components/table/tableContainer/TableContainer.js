4334ca81d5be9d619bf26f73a3de1b74d97cc8a8
asosunoff <al.sosunoff@gmail.com>
2020-10-08 19:08:16 +0300

    Fix

--- src/components/table/tableContainer/TableContainer.js
+++ src/components/table/tableContainer/TableContainer.js
@@ -1,17 +1,17 @@
 import React, { useMemo } from "react";
 import styles from "./TableContainer.module.css";
 
-const TableContainer = ({ header, btnsLength = 0, children }) => {
+const TableContainer = ({ header, rowsBtnLength = 0, children }) => {
 	const memoizedWidthColumn = useMemo(() => {
 		const FILTER_BTN_COUNT = 1;
 
 		return {
 			"--width-column": Object.values(header)
 				.map(({ width = "1fr" }) => width)
-				.concat(Array(btnsLength || FILTER_BTN_COUNT).fill("50px"))
+				.concat(Array(rowsBtnLength || FILTER_BTN_COUNT).fill("50px"))
 				.join(" "),
 		};
-	}, [header, btnsLength]);
+	}, [header, rowsBtnLength]);
 
 	return (
 		<div className={styles.table} style={memoizedWidthColumn}>
