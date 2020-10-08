3c326574762e74889b025ca0fc60636cf886ea43
asosunoff <al.sosunoff@gmail.com>
2020-10-08 19:08:58 +0300

    import material icons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from "./components/app";
 import "./reset.css";
 import "./variable.css";
 import "./styles.css";
-
+import "material-icons/iconfont/material-icons.css";
 // <React.StrictMode>
 ReactDOM.render(<App />, document.getElementById("root"));
