ad166cf3641ff966f5a79b191c87981f5668403c
asosunoff <al.sosunoff@gmail.com>
2020-10-08 10:17:44 +0300

    Paging component is fixed

--- src/components/table/paging/Paging.js
+++ src/components/table/paging/Paging.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Paging.module.css";
+import cn from "classnames";
 
 const Paging = ({ pageCount, pageCurrent, setPageHandler }) => {
 	const [pageCurrentLocal, setPageLocal] = useState(pageCurrent);
+
+	const isDisable = !Boolean(pageCount);
 
 	useEffect(() => {
 		setPageLocal(pageCurrent);
 	}, [pageCurrent]);
 
 	const onFirstPage = () => {
+		if (isDisable) {
+			return;
+		}
 		setPageHandler(1);
 	};
 
 	const onPrevPage = () => {
+		if (isDisable) {
+			return;
+		}
 		if (pageCurrent - 1 === 0) {
 			return;
 		}
@@ -20,6 +29,9 @@ const Paging = ({ pageCount, pageCurrent, setPageHandler }) => {
 	};
 
 	const onCurrentPage = ({ target }) => {
+		if (isDisable) {
+			return;
+		}
 		const page = Number(target.value);
 		if (page < 1 || page > pageCount) {
 			setPageLocal("");
@@ -30,6 +42,9 @@ const Paging = ({ pageCount, pageCurrent, setPageHandler }) => {
 	};
 
 	const onNextPage = () => {
+		if (isDisable) {
+			return;
+		}
 		if (pageCurrent + 1 > pageCount) {
 			return;
 		}
@@ -37,41 +52,40 @@ const Paging = ({ pageCount, pageCurrent, setPageHandler }) => {
 	};
 
 	const onLastPage = () => {
+		if (isDisable) {
+			return;
+		}
 		setPageHandler(pageCount);
 	};
 
 	return (
-		<>
-			{pageCount ? (
-				<div className={styles.paging}>
-					<div className={styles["first-page"]} onClick={onFirstPage}>
-						первая
-					</div>
-					<div className={styles["box-command"]}>
-						<div className={styles.prev} onClick={onPrevPage}></div>
-						<input
-							type="text"
-							onChange={onCurrentPage}
-							value={pageCurrentLocal}
-						/>
-						<div className={styles.next} onClick={onNextPage}></div>
-					</div>
-					<div className={styles["last-page"]} onClick={onLastPage}>
-						последняя
-					</div>
-				</div>
-			) : (
-				<div className={styles["paging-disable"]}>
-					<div className={styles["first-page"]}>первая</div>
-					<div className={styles["box-command"]}>
-						<div className={styles.prev}></div>
-						<input type="text" value={pageCurrentLocal} disabled />
-						<div className={styles.next}></div>
-					</div>
-					<div className={styles["last-page"]}>последняя</div>
-				</div>
-			)}
-		</>
+		<div
+			className={cn({
+				[styles.paging]: Boolean(pageCount),
+				[styles["paging-disable"]]: !Boolean(pageCount),
+			})}
+		>
+			<div className={styles["first-page"]} onClick={onFirstPage}>
+				первая
+			</div>
+
+			<div className={styles["box-command"]}>
+				<div className={styles.prev} onClick={onPrevPage}></div>
+
+				<input
+					type="text"
+					onChange={onCurrentPage}
+					value={pageCurrentLocal}
+					disabled={Boolean(pageCount) ? "" : null}
+				/>
+
+				<div className={styles.next} onClick={onNextPage}></div>
+			</div>
+
+			<div className={styles["last-page"]} onClick={onLastPage}>
+				последняя
+			</div>
+		</div>
 	);
 };
 
