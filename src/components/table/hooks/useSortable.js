ba319fabec3b2b8552de17cc9cc18b18589d7302
asosunoff <al.sosunoff@gmail.com>
2020-10-10 12:51:28 +0300

    useSortable is fix

--- src/components/table/hooks/useSortable.js
+++ src/components/table/hooks/useSortable.js
@@ -2,11 +2,11 @@ import { cloneDeep } from "lodash";
 import { useMemo } from "react";
 import sorting from "../utils/sorting";
 
-export const useSorting = (list, prop, order, sortingType) =>
+export const useSorting = (list, prop, direction, sortingType) =>
 	useMemo(
 		() =>
 			cloneDeep(list).sort((a, b) =>
-				sorting(sortingType, order, a[prop], b[prop])
+				sorting(sortingType, direction, a[prop], b[prop])
 			),
-		[list, prop, order, sortingType]
+		[list, prop, direction, sortingType]
 	);
