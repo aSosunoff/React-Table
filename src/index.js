a17c0bba47a27877fb9d29587ce94c0e66e56d2a
asosunoff <al.sosunoff@gmail.com>
2020-11-14 14:37:11 +0300

    css to scss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
-import "./reset.css";
-import "./variable.css";
-import "./styles.css";
-import "material-icons/iconfont/material-icons.css";
-// <React.StrictMode>
+import "./reset.scss";
+import "./variable.scss";
+import "./styles.scss";
+import "material-icons/iconfont/material-icons.scss";
+
 ReactDOM.render(<App />, document.getElementById("root"));
