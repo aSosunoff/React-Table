7002fed53cf8268ca5e9b1c4f11f91c61f23e6f0
asosunoff <al.sosunoff@gmail.com>
2020-11-21 11:28:22 +0300

    Fix app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,143 +1,121 @@
 import { cloneDeep } from "lodash";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Table from "../table/components/Table";
 import { v4 } from "uuid";
 
-function App() {
-	const [list, setList] = useState([
-		{
-			id: 2,
-			text: "aa",
-			name: "bb",
-		},
-		{
-			id: 1,
-			text: "a",
-			name: "b",
-		},
-		{
-			id: 3,
-			text: "aaa",
-			name: "bbb",
-		},
-		{
-			id: 4,
-			text: "aaaa",
-			name: "bbbb",
-		},
-		{
-			id: 5,
-			text: "aaaaa",
-			name: "bbbbb",
-		},
-		/* {
-			id: 6,
-			text: "aaaaaa",
-			name: "bbbbbb",
-		}, */
-	]);
+// utils
+const newRecord = (id, text, name, date) => ({
+  id,
+  text,
+  name,
+  date,
+});
 
-	const added = () => {
-		const value = v4();
-		setList([...list, { id: value, text: value, name: "test" }]);
-	};
+const App = () => {
+  const [list, setList] = useState([
+    newRecord(2, "aa", "bb", new Date(2020, 1)),
+    newRecord(1, "a", "b", new Date(2020, 0)),
+    newRecord(3, "aaa", "bbb", new Date(2020, 2)),
+  ]);
 
-	const deleteRow = (record) => {
-		const listLocal = cloneDeep(list);
-		setList([...listLocal.filter(({ id }) => id !== record.id)]);
-	};
+  const addRecord = useCallback(() => {
+    const value = v4();
+    setList((prev) => [...prev, newRecord(value, value, "test", new Date())]);
+  }, []);
 
-	const header = {
-		id: {
-			titleHead: "№",
-			/* width: "70px", */
-			order: {
-				type: "number",
-				direction: "asc",
-			},
-			filter: {
-				type: "list",
-				items: [
-					{ id: 1, text: 1 },
-					{ id: 2, text: 2 },
-				],
-			},
-		},
-		text: {
-			titleHead: "текст",
-			order: {
-				type: "string",
-				direction: "desc",
-			},
-			filter: {
-				type: "text",
-				detail: {
-					name: 12,
-					qwe: "qwe",
-				},
-			},
-			btns: [
-				(record) =>
-					record.id === 3 && {
-						/* title: "Добавить запись", */
-						title: ({ id }) => id,
-						handler: deleteRow,
-						icon: "delete",
-						/* disabled: true, */
-					},
-			],
-		},
-		name: {
-			titleHead: "имя",
-			/* format: (_, record) => record.text, */
-			filter: {
-				type: "button",
-				icon: "search",
-				hundler: (cbFilter, element) => {
-					setTimeout(() => {
-						cbFilter('bbb', 'asd');
-					}, 2000)
-				},
-				detail: {
-					name: 12,
-					qwe: "qwe",
-				},
-			},
-		},
-	};
+  const deleteRecord = useCallback(
+    (record) =>
+      setList((prev) => [...prev.filter(({ id }) => id !== record.id)]),
+    []
+  );
 
-	return (
-		<>
-			<Table
-				title="Таблица"
-				/* rowCssClass={(record) => (record.id === 3 ? "qweqwe" : "")} */
-				list={list}
-				header={header}
-				pageSize={10}
-				rowsBtn={[
-					{
-						/* title: "Добавить запись", */
-						title: ({ id }) => id,
-						handler: deleteRow,
-						icon: "delete",
-						/* disabled: true, */
-					},
-				]}
-				controlPanel={[
-					{
-						title: "Добавить запись",
-						handler: added,
-					},
-				]}
-				onRowClick={(record) => {
+  const header = {
+    id: {
+      titleHead: "№",
+      width: "170px",
+      order: {
+        type: "number",
+        direction: "asc",
+      },
+      filter: {
+        type: "list",
+        items: [
+          { id: 1, text: 1 },
+          { id: 2, text: 2 },
+        ],
+      },
+    },
+    text: {
+      titleHead: "Текст",
+      order: {
+        type: "string",
+        direction: "desc",
+      },
+      filter: {
+        type: "text",
+        detail: {
+          name: 12,
+          qwe: "qwe",
+        },
+      },
+      btns: [
+        (record) =>
+          record.id === 3 && {
+            title: ({ id }) => `Удалить запись ${id}`,
+            handler: deleteRecord,
+            icon: "delete",
+          },
+      ],
+    },
+    name: {
+      titleHead: "Наименование",
+      /* filter: {
+        type: "button",
+        icon: "search",
+        hundler: (cbFilter, element) => {
+          setTimeout(() => {
+            cbFilter("bbb", "asd");
+          }, 2000);
+        },
+        detail: {
+          name: 12,
+          qwe: "qwe",
+        },
+      }, */
+    },
+  };
+
+  return (
+    <>
+      <Table
+        title="Таблица"
+        list={list}
+        header={header}
+        pageSize={10}
+        // rowsBtn={[
+        // 	{
+        // 		/* title: "Добавить запись", */
+        // 		title: ({ id }) => id,
+        // 		handler: deleteRecord,
+        // 		icon: "delete",
+        // 		/* disabled: true, */
+        // 	},
+        // ]}
+        controlPanel={[
+          {
+            title: "Добавить запись",
+            handler: addRecord,
+          },
+        ]}
+        /* onRowClick={(record) => {
 					console.log("onRowClick", record);
 				}}
 				onUnselectRecord={() => {
 					console.log("onUnselectRecord");
-				}}
-			></Table>
-		</>
-	);
-}
+				}} */
+      ></Table>
+    </>
+  );
+};
 
 export default App;
