289d1a777827b72a9f6d8aa35d383fc07b9367e9
asosunoff <al.sosunoff@gmail.com>
2020-11-22 01:25:48 +0300

    Body fix

--- src/components/table/components/body/Body.js
+++ src/components/table/components/body/Body.js
@@ -6,70 +6,69 @@ import styles from "./Body.module.scss";
 import Row from "./row";
 
 const Body = ({
-	rowCssClass,
-	list = [],
-	header,
-	rowsBtn = [],
-	selectedRowId,
-	onRowClick,
+  rowCssClass,
+  list = [],
+  header,
+  rowsBtn = [],
+  selectedRowId,
+  onRowClick,
 }) => {
-	const row = useCallback(
-		(record) =>
-			Object.keys(header).map((key) => {
-				const btns = cloneDeep(header[key].btns || []);
-				const {
-					format = () => record[key],
-					cssClass = () => null,
-					titleCell = null,
-					clickHandler = () => null,
-				} = header[key];
+  const row = useCallback(
+    (record) =>
+      Object.keys(header).map((key) => {
+        const btns = cloneDeep(header[key].btns || []);
+        const {
+          format = () => record[key],
+          cssClass = () => null,
+          titleCell = null,
+          clickHandler = () => null,
+        } = header[key];
 
-				let attributes = {};
+        let attributes = {};
 
-				if (titleCell === true) {
-					attributes = { ...attributes, title: record[key] };
-				} else if (typeof titleCell === "function") {
-					attributes = { ...attributes, title: titleCell(record[key], record) };
-				}
+        if (titleCell === true) {
+          attributes = { ...attributes, title: record[key] };
+        } else if (typeof titleCell === "function") {
+          attributes = { ...attributes, title: titleCell(record[key], record) };
+        }
 
-				return {
-					key,
-					value: format(record[key], record),
-					btns: btns.map((btn) => ({
-						uuid: v4(),
-						btn,
-					})),
-					cssClass: cssClass(record[key], key, record),
-					attributes,
-					clickHandler: () => clickHandler(record[key], record),
-				};
-			}),
-		[header]
-	);
+        return {
+          key,
+          value: format(record[key], record),
+          btns: btns.map((btn) => ({
+            uuid: v4(),
+            btn,
+          })),
+          cssClass: cssClass(record[key], key, record),
+          attributes,
+          clickHandler: () => clickHandler(record[key], record),
+        };
+      }),
+    [header]
+  );
 
-	const rowHandler = useCallback(
-		(indexRecord, record) => {
-			onRowClick(isEmptyObject(record) ? null : indexRecord, record);
-		},
-		[onRowClick]
-	);
+  const rowHandler = useCallback(
+    (indexRecord, record) =>
+      onRowClick(isEmptyObject(record) ? null : indexRecord, record),
+    [onRowClick]
+  );
 
-	return (
-		<div className={styles.table__body}>
-			{list.map(({ uuid, ...record }, indexRecord) => (
-				<Row
-					key={uuid}
-					row={row(record)}
-					rowsBtn={rowsBtn}
-					indexRecord={indexRecord}
-					isSelected={selectedRowId === indexRecord}
-					rowCssClass={rowCssClass}
-					record={record}
-					onRowClick={() => rowHandler(indexRecord, record)}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className={styles.table__body}>
+      {list.map(({ uuid, ...record }, indexRecord) => (
+        <Row
+          key={uuid}
+          row={row(record)}
+          rowsBtn={rowsBtn}
+          indexRecord={indexRecord}
+          isSelected={selectedRowId === indexRecord}
+          rowCssClass={rowCssClass}
+          record={record}
+          onRowClick={() => rowHandler(indexRecord, record)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Body;
