bb6c1e4ed5ef89e47b5efb1f2052da153f4ace65
asosunoff <al.sosunoff@gmail.com>
2020-11-21 15:58:23 +0300

    Filter fix

--- src/components/table/components/filter/Filter.js
+++ src/components/table/components/filter/Filter.js
@@ -6,77 +6,74 @@ import List from "./list/List";
 import Button from "./button/Button";
 
 const Filter = ({
-	filterState,
-	filterPanel,
-	onSetFilter = () => null,
-	onDeleteFromFilterByField = () => null,
-	onClearFilter = () => null,
+  filterState,
+  filterPanel,
+  onSetFilter = () => null,
+  onDeleteFromFilterByField = () => null,
+  onClearFilter = () => null,
 } = {}) => {
-	const filter = useRef(null);
+  const filter = useRef(null);
 
-	const filterRow = useMemo(() => Object.entries(filterPanel), [filterPanel]);
+  const filterRow = useMemo(() => Object.entries(filterPanel), [filterPanel]);
 
-	const getValueFromFilter = (field) =>
-		(filterState[field] && filterState[field].value) || "";
+  return (
+    <div ref={filter} className={styles.table__filter}>
+      {filterRow.map(([field, { type, items, icon, handler }]) => {
+        switch (type) {
+          case "text":
+            return (
+              <Text
+                key={field}
+                clsMain={styles.table__cell_filter}
+                clsButton={styles.table__cell_button}
+                value={filterState[field]?.value ?? ""}
+                onSet={onSetFilter.bind(this, field)}
+                onClear={onDeleteFromFilterByField.bind(this, field)}
+              />
+            );
+          case "list":
+            return (
+              <List
+                key={field}
+                clsMain={styles.table__cell_filter}
+                clsButton={styles.table__cell_button}
+                value={filterState[field]?.value ?? -1}
+                items={items}
+                onSet={onSetFilter.bind(this, field)}
+                onClear={onDeleteFromFilterByField.bind(this, field)}
+              />
+            );
+          case "button":
+            return (
+              <Button
+                key={field}
+                clsMain={styles.table__cell_filter}
+                clsButton={styles.table__cell_button}
+                icon={icon}
+                value={filterState[field]}
+                handler={handler}
+                onSet={onSetFilter.bind(this, field)}
+                onClear={onDeleteFromFilterByField.bind(this, field)}
+              />
+            );
+          default:
+            return (
+              <div className={styles.table__cell_filter} key={field}></div>
+            );
+        }
+      })}
 
-	return (
-		<div ref={filter} className={styles.table__filter}>
-			{filterRow.map(([field, { type, items, icon, hundler }]) => {
-				switch (type) {
-					case "text":
-						return (
-							<Text
-								key={field}
-								clsMain={styles.table__cell_filter}
-								clsButton={styles.table__cell_button}
-								value={getValueFromFilter(field)}
-								onSet={onSetFilter.bind(this, field)}
-								onClear={onDeleteFromFilterByField.bind(this, field)}
-							/>
-						);
-					case "list":
-						return (
-							<List
-								key={field}
-								clsMain={styles.table__cell_filter}
-								clsButton={styles.table__cell_button}
-								value={getValueFromFilter(field)}
-								items={items}
-								onSet={onSetFilter.bind(this, field)}
-								onClear={onDeleteFromFilterByField.bind(this, field)}
-							/>
-						);
-					case "button":
-						return (
-							<Button
-								key={field}
-								clsMain={styles.table__cell_filter}
-								clsButton={styles.table__cell_button}
-								icon={icon}
-								value={getValueFromFilter(field)}
-								hundler={hundler}
-								onSet={onSetFilter.bind(this, field)}
-								onClear={onDeleteFromFilterByField.bind(this, field)}
-							/>
-						);
-					default:
-						return (
-							<div className={styles.table__cell_filter} key={field}></div>
-						);
-				}
-			})}
-
-			<div
-				className={cn([
-					styles.table__cell_filter,
-					styles["table__cell_btn-delete"],
-				])}
-				onClick={onClearFilter}
-			>
-				<i className="material-icons">delete</i>
-			</div>
-		</div>
-	);
+      <div
+        className={cn([
+          styles.table__cell_filter,
+          styles["table__cell_btn-delete"],
+        ])}
+        onClick={onClearFilter}
+      >
+        <i className="material-icons">delete</i>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
