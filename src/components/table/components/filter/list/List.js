3495b45e656badb8301cc979688645974b504ffb
asosunoff <al.sosunoff@gmail.com>
2020-11-23 00:26:29 +0300

    Added List test

--- src/components/table/components/filter/list/List.js
+++ src/components/table/components/filter/list/List.js
@@ -1,57 +1,66 @@
 import React from "react";
 import styles from "./List.module.scss";
 import cn from "classnames";
+import PropTypes from "prop-types";
 
-const List = ({
-	clsMain,
-	clsButton,
-	items,
-	value,
-	onSet = () => {},
-	onClear = () => {},
-} = {}) => {
-	const isValue = Boolean(value);
+const List = ({ clsMain, clsButton, items, value, onSet, onClear }) => {
+  const isValue = Boolean(value);
 
-	const changeHandler = ({ target }) => {
-		const selected = target.options[target.options.selectedIndex];
+  const changeHandler = ({ target }) => {
+    const selected = target.options[target.options.selectedIndex];
 
-		if (selected.value === "-1") {
-			onClear();
-		} else {
-			onSet(selected.value, {
-				obj: selected.dataset.obj && JSON.parse(selected.dataset.obj),
-			});
-		}
-	};
+    if (selected.value === "-1") {
+      onClear();
+    } else {
+      onSet(selected.value, {
+        obj: selected.dataset.obj && JSON.parse(selected.dataset.obj),
+      });
+    }
+  };
 
-	const clearHandler = ({ target }) => {
-		target.previousElementSibling.selectedIndex = 0;
-		onClear();
-	};
+  const clearHandler = ({ target }) => {
+    target.previousElementSibling.selectedIndex = 0;
+    onClear();
+  };
 
-	return (
-		<div
-			className={cn([clsMain, styles.table__cell_select])}
-			style={{
-				"--button-delete": isValue ? 1 : 0,
-			}}
-		>
-			<select value={value} onChange={changeHandler}>
-				<option value="-1">Выберите</option>
-				{items.map((item, inx) => (
-					<option key={inx} value={item.id} data-obj={JSON.stringify(item)}>
-						{item.text}
-					</option>
-				))}
-			</select>
+  return (
+    <div
+      className={cn([clsMain, styles.table__cell_select])}
+      style={{
+        "--button-delete": isValue ? 1 : 0,
+      }}
+    >
+      <select value={value} onChange={changeHandler}>
+        <option value="-1">Выберите</option>
+        {items.map((item, inx) => (
+          <option key={inx} value={item.id} data-obj={JSON.stringify(item)}>
+            {item.text}
+          </option>
+        ))}
+      </select>
 
-			{isValue ? (
-				<i className={cn(["material-icons", clsButton])} onClick={clearHandler}>
-					clear
-				</i>
-			) : null}
-		</div>
-	);
+      {isValue ? (
+        <i className={cn(["material-icons", clsButton])} onClick={clearHandler}>
+          clear
+        </i>
+      ) : null}
+    </div>
+  );
+};
+
+List.defaultProps = {
+  items: [],
+  onSet: () => null,
+  onClear: () => null,
+};
+
+List.propTypes = {
+  clsMain: PropTypes.string,
+  clsButton: PropTypes.string,
+  items: PropTypes.array,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onSet: PropTypes.func,
+  onClear: PropTypes.func,
 };
 
 export default List;
---
+++ src/components/table/components/filter/list/List.test.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { mount } from "enzyme";
+import List from "./List";
+
+/* jest.mock("react"); */
+
+describe("List", () => {
+  let wrapper;
+
+  /* const getReactMock = () => require("react");
+  const getRequireActual = () => jest.requireActual("react"); */
+
+  const getByDataId = (wrapper, dataId) =>
+    wrapper.find(`[data-test-id="${dataId}"]`);
+
+  /* const Input = () => getByDataId(wrapper, "input");
+  const ClearButton = () => getByDataId(wrapper, "text-clear-button"); */
+
+  beforeEach(() => {
+    /* const reactMock = getReactMock();
+    const reactActual = getRequireActual();
+
+    for (const key in reactMock) {
+      if (reactMock[key].mockImplementation) {
+        reactMock[key].mockImplementation(reactActual[key]);
+      }
+    } */
+
+    wrapper = mount(<List />);
+  });
+
+  it("should render", () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  /* it("should be render hide clear button after beginning", () => {
+    expect(ClearButton()).toHaveLength(0);
+  });
+
+  it("should be render show clear button after beginning", () => {
+    wrapper = mount(<Text value="1" />);
+
+    expect(ClearButton()).toHaveLength(1);
+  });
+
+  it("should be call onSet after keyup Enter", () => {
+    const onSet = jest.fn();
+    const value = 2;
+    wrapper.setProps({
+      onSet,
+    });
+    Input().simulate("keyup", { key: "Enter", target: { value } });
+    const [[page]] = onSet.mock.calls;
+    expect(page).toBe(value);
+  });
+
+  it("should be set local state", () => {
+    const setValueLocal = jest.fn();
+    getReactMock().useState.mockImplementation((value) => [
+      value,
+      setValueLocal,
+    ]);
+
+    getReactMock().useEffect.mockImplementation(() => null);
+
+    wrapper = mount(<Text />);
+
+    const value = 2;
+
+    Input().simulate("change", { target: { value } });
+    const [[valueLocal]] = setValueLocal.mock.calls;
+    expect(valueLocal).toBe(value);
+  });
+
+  it("should be call onClear and clear local value", () => {
+    const setValueLocal = jest.fn();
+    const onClear = jest.fn();
+
+    getReactMock().useState.mockImplementation((value) => [
+      value,
+      setValueLocal,
+    ]);
+
+    getReactMock().useEffect.mockImplementation(() => null);
+
+    wrapper = mount(<Text value="1" onClear={onClear} />);
+
+    ClearButton().simulate("click");
+
+    expect(onClear).toHaveBeenCalled();
+
+    const [[result]] = setValueLocal.mock.calls;
+    expect(result).toBe("");
+  }); */
+});
