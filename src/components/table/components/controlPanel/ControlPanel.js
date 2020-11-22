5a5e4564dd4de4c8ad7b543f986754a742e91256
asosunoff <al.sosunoff@gmail.com>
2020-11-22 18:50:20 +0300

    Added test to ControlPanel

--- src/components/table/components/controlPanel/ControlPanel.js
+++ src/components/table/components/controlPanel/ControlPanel.js
@@ -2,42 +2,45 @@ import React, { useMemo } from "react";
 import styles from "./ControlPanel.module.scss";
 import { v4 } from "uuid";
 import cn from "classnames";
+import PropTypes from "prop-types";
 
-const ControlPanel = ({ controlPanel = [] } = {}) => {
-	const isControlPanel = Boolean(controlPanel.length);
+const ControlPanel = ({ controlPanel }) => {
+  const isControlPanel = Boolean(controlPanel.length);
 
-	const controlPanelLocal = useMemo(
-		() =>
-			controlPanel.map((record) => ({
-				uuid: v4(),
-				...record,
-			})),
-		[controlPanel]
-	);
+  const controlPanelLocal = useMemo(
+    () =>
+      controlPanel.map((record) => ({
+        uuid: v4(),
+        ...record,
+      })),
+    [controlPanel]
+  );
 
-	const buttonHandler = (btn, event) => {
-		if (!btn.disabled) {
-			btn.handler(event);
-		}
-	};
+  return (
+    <>
+      {isControlPanel ? (
+        <div className={styles["button-control"]} data-test-id="control-panel">
+          {controlPanelLocal.map(({ uuid, ...btn }) => (
+            <div
+              className={cn([styles.button, { dissabled: btn.disabled }])}
+              key={uuid}
+              onClick={(event) => !btn.disabled && btn.handler(event)}
+            >
+              {btn.title}
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
+};
 
-	return (
-		<>
-			{isControlPanel ? (
-				<div className={styles["button-control"]}>
-					{controlPanelLocal.map(({ uuid, ...btn }) => (
-						<div
-							className={cn([styles.button, { dissabled: btn.disabled }])}
-							key={uuid}
-							onClick={buttonHandler.bind(this, btn)}
-						>
-							{btn.title}
-						</div>
-					))}
-				</div>
-			) : null}
-		</>
-	);
+ControlPanel.defaultProps = {
+  controlPanel: [],
+};
+
+ControlPanel.propTypes = {
+  controlPanel: PropTypes.array,
 };
 
 export default ControlPanel;
