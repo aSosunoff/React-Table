9ff3337e7440aa5131c7818132ec4a677809c787
asosunoff <al.sosunoff@gmail.com>
2020-11-23 21:56:26 +0300

    Button added test

--- src/components/table/components/filter/button/Button.js
+++ src/components/table/components/filter/button/Button.js
@@ -1,25 +1,29 @@
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 import styles from "./Button.module.scss";
 import cn from "classnames";
+import PropTypes from "prop-types";
 
 const Button = ({
   clsMain,
   clsButton,
   icon,
   value,
-  handler = () => {},
-  onSet = () => {},
-  onClear = () => {},
-} = {}) => {
-  const isValue = Boolean(value?.value);
+  handler,
+  onSet,
+  onClear,
+}) => {
+  const isValue = useMemo(() => Boolean(value?.value), [value]);
 
   const title = useMemo(() => value?.title || value?.value || "", [value]);
 
-  const changeHandler = ({ target }) => {
-    handler((value, additionalProperties) => {
-      onSet(value, additionalProperties);
-    }, target);
-  };
+  const changeHandler = useCallback(
+    ({ target }) => {
+      handler((value, additionalProperties) => {
+        onSet(value, additionalProperties);
+      }, target);
+    },
+    [handler, onSet]
+  );
 
   return (
     <div
@@ -28,19 +32,49 @@ const Button = ({
         "--button-delete": isValue ? 1 : 0,
       }}
     >
-      <div className={styles.table__cell_input}>{`${title}`}</div>
+      <div
+        className={styles.table__cell_input}
+        data-test-id="button-title"
+      >{`${title}`}</div>
 
-      <i className={cn(["material-icons", clsButton])} onClick={changeHandler}>
+      <i
+        className={cn(["material-icons", clsButton])}
+        onClick={changeHandler}
+        data-test-id="button-icon"
+      >
         {icon}
       </i>
 
       {isValue ? (
-        <i className={cn(["material-icons", clsButton])} onClick={onClear}>
+        <i
+          className={cn(["material-icons", clsButton])}
+          onClick={onClear}
+          data-test-id="button-clear-button"
+        >
           clear
         </i>
       ) : null}
     </div>
   );
+};
+
+Button.defaultProps = {
+  handler: () => null,
+  onSet: () => null,
+  onClear: () => null,
+};
+
+Button.propTypes = {
+  clsMain: PropTypes.string,
+  clsButton: PropTypes.string,
+  icon: PropTypes.string,
+  value: PropTypes.shape({
+    value: PropTypes.any,
+    title: PropTypes.string,
+  }),
+  handler: PropTypes.func,
+  onSet: PropTypes.func,
+  onClear: PropTypes.func,
 };
 
 export default Button;
---
+++ src/components/table/components/filter/button/Button.test.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { mount } from "enzyme";
+import Button from "./Button";
+
+describe("Button", () => {
+  let wrapper;
+
+  const getByDataId = (wrapper, dataId) =>
+    wrapper.find(`[data-test-id="${dataId}"]`);
+
+  /* const Select = () => wrapper.find("select"); */
+  const ClearButton = () => getByDataId(wrapper, "button-clear-button");
+  const Title = () => getByDataId(wrapper, "button-title");
+  const Icon = () => getByDataId(wrapper, "button-icon");
+
+  beforeEach(() => {
+    wrapper = mount(<Button />);
+  });
+
+  it("should render", () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it("should be render hide clear button after beginning", () => {
+    expect(ClearButton()).toHaveLength(0);
+  });
+
+  it("should be render show clear button after beginning", () => {
+    wrapper.setProps({
+      value: {
+        value: 1,
+      },
+    });
+
+    expect(ClearButton()).toHaveLength(1);
+  });
+
+  it("should be contain title by value", () => {
+    const value = {
+      value: "1",
+    };
+
+    wrapper.setProps({
+      value,
+    });
+
+    expect(Title().text()).toBe(value.value);
+  });
+
+  it("should be contain title by title", () => {
+    const value = {
+      value: "1",
+      title: "test",
+    };
+
+    wrapper.setProps({
+      value,
+    });
+
+    expect(Title().text()).toBe(value.title);
+  });
+
+  it("should be call handler after click to icon button and call onSet aftes call handler", () => {
+    const target = 1;
+    const handler = jest.fn();
+    const onSet = jest.fn();
+
+    const value = 1;
+    const additionalProperties = 2;
+
+    wrapper.setProps({
+      handler,
+      onSet,
+    });
+
+    Icon().simulate("click", {
+      target,
+    });
+
+    expect(handler).toHaveBeenCalled();
+    const [[callback, resultTarget]] = handler.mock.calls;
+    expect(resultTarget).toBe(target);
+
+    callback(value, additionalProperties);
+
+    const [[resultValue, resultAdditionalProperties]] = onSet.mock.calls;
+    expect(resultValue).toBe(value);
+    expect(resultAdditionalProperties).toBe(additionalProperties);
+  });
+});
