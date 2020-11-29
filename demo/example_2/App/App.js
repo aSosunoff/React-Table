9eca06362c5d0fb137917092db2e51a3e3a918c4
asosunoff <al.sosunoff@gmail.com>
2020-11-29 23:58:15 +0300

    Added example 2

---
+++ demo/example_2/App/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Table from "../../../src";
+import "material-icons/iconfont/material-icons.scss";
+
+/* utils */
+const newRecord = (id, text, name, date) => ({
+  id,
+  text,
+  name,
+  date,
+});
+/*  */
+
+const App = () => {
+  const [list, setList] = useState([
+    newRecord(2, "aa", "bb", new Date(2020, 10, 10).getTime()),
+    newRecord(1, "a", "b", new Date(2020, 10, 1).getTime()),
+    newRecord(3, "aaa", "bbb", new Date(2020, 10, 15).getTime()),
+  ]);
+
+  return (
+    <Table
+      title="Таблица"
+      list={list}
+      header={{
+        avatar: {
+          width: "100px",
+          format: () => (
+            <img
+              alt="avatar"
+              style={{
+                maxWidth: "70px",
+              }}
+              src="https://avatars2.githubusercontent.com/u/15358461?v=4"
+            />
+          ),
+        },
+        id: {},
+      }}
+      pageSize={10}
+      custom
+      onFilterHandler={(filter) => console.log(filter)}
+      onOrderHandler={(...order) => console.log(order)}
+      onPageHandler={(page) => console.log(page)}
+      pageCount={1}
+      currentPage={1}
+    />
+  );
+};
+
+export default App;
---
+++ demo/example_2/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./reset.scss";
+import "./styles.scss";
+
+ReactDOM.render(<App />, document.getElementById("root"));
