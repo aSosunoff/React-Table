99ba817492d9fa6dee9d11310a117073e332462d
asosunoff <al.sosunoff@gmail.com>
2020-12-02 01:12:51 +0300

    App fix

--- demo/example_2/App/App.js
+++ demo/example_2/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import Table from "../../../src";
 import "material-icons/iconfont/material-icons.scss";
 
@@ -8,6 +8,7 @@ const newRecord = (id, text, name, date) => ({
   text,
   name,
   date,
+  avatar: "https://avatars2.githubusercontent.com/u/15358461?v=4",
 });
 /*  */
 
@@ -16,23 +17,49 @@ const App = () => {
     newRecord(2, "aa", "bb", new Date(2020, 10, 10).getTime()),
     newRecord(1, "a", "b", new Date(2020, 10, 1).getTime()),
     newRecord(3, "aaa", "bbb", new Date(2020, 10, 15).getTime()),
+    newRecord(5, "aaa", "bbb", new Date(2020, 10, 15).getTime()),
+    newRecord(4, "aaa", "bbb", new Date(2020, 10, 15).getTime()),
+    newRecord(6, "aaa", "bbb", new Date(2020, 10, 15).getTime()),
+    newRecord(7, "aaa", "bbb", new Date(2020, 10, 15).getTime()),
   ]);
+
+  const [currentPage, setPageHandler] = useState(1);
+  const pageSize = 5;
+  const pageCount = Math.ceil(list.length / pageSize);
+
+  const onOrderHandler = useCallback(
+    (prop, direction) => console.log(prop, direction),
+    []
+  );
+  const onFilterHandler = useCallback((filter) => console.log(filter), []);
+
+  const itemsOnPage = useMemo(
+    () =>
+      list.slice(
+        (currentPage - 1) * pageSize,
+        (currentPage - 1) * pageSize + pageSize
+      ),
+    [currentPage, list]
+  );
 
   return (
     <Table
       title="Таблица"
-      list={list}
+      list={itemsOnPage}
+      recordStyles={{
+        height: "71px",
+      }}
       header={{
         avatar: {
           width: "100px",
-          format: (value, record) =>
-            record.id && (
+          format: (value) =>
+            value && (
               <img
                 alt="avatar"
                 style={{
                   maxWidth: "70px",
                 }}
-                src="https://avatars2.githubusercontent.com/u/15358461?v=4"
+                src={value}
               />
             ),
         },
@@ -45,13 +72,13 @@ const App = () => {
           },
         },
       }}
-      pageSize={5}
+      pageSize={pageSize}
       custom
-      onFilterHandler={(filter) => console.log(filter)}
-      onOrderHandler={(prop, direction) => console.log(prop, direction)}
-      onPageHandler={(page) => console.log(page)}
-      pageCount={2}
-      currentPage={1}
+      onFilterHandler={onFilterHandler}
+      onOrderHandler={onOrderHandler}
+      onPageHandler={setPageHandler}
+      pageCount={pageCount}
+      currentPage={currentPage}
     />
   );
 };
